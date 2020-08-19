c2375de1bd743cd0ce2984cd6b1a38cb204f46f2
shreyaghosh16 <shreyaghosh95@gmail.com>
2020-08-19 11:45:31 +0530

    UC-1

--- EmpWage.java
+++ EmpWage.java
@@ -2,10 +2,42 @@
 public class EmpWage {
 	
     public static void main(String[] args) {
-		
-		   System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+	       System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		   
+		   int IS_FULL_TIME = 1;
+	
+	   double empCheck = Math.floor(Math.random() * 10) % 2;
+	   if (empCheck == IS_FULL_TIME)
+		    System.out.println("Employee is Present");
+	   else
+		    System.out.println("Employee is Absent");
 		   
 	
     }
 
-}
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
