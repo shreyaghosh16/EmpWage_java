d421b0bf801fc9873eb843bab3c11d0a0a1d40d7
shreyaghosh16 <shreyaghosh95@gmail.com>
2020-08-19 11:48:16 +0530

    UC-2

--- EmpWage.java
+++ EmpWage.java
@@ -5,12 +5,18 @@ public class EmpWage {
 	       System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 		   
 		   int IS_FULL_TIME = 1;
+		   int EMP_RATE_PER_HOUR = 20;
+		   
+		   int empHrs = 0;
+		   int empWage = 0;
 	
 	   double empCheck = Math.floor(Math.random() * 10) % 2;
-	   if (empCheck == IS_FULL_TIME)
-		    System.out.println("Employee is Present");
-	   else
-		    System.out.println("Employee is Absent");
+		   if (empCheck == IS_FULL_TIME)
+		       empHrs = 8;
+	       else
+			   empHrs = 0;
+		       empWage = empHrs * EMP_RATE_PER_HOUR;
+		       System.out.println("Emp Wage: " + empWage);
 		   
 	
     }
