cf6b00c29bcfc4504da8c25ae8f495740856754a
shreyaghosh16 <shreyaghosh95@gmail.com>
2020-08-19 11:39:22 +0530

    Welcome to EmpWage Computation

---
+++ EmpWage.java
@@ -0,0 +1,11 @@
+
+public class EmpWage {
+	
+    public static void main(String[] args) {
+		
+		   System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		   
+	
+    }
+
+}
